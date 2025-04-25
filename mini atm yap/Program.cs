6ff30f0f8e28cfbe32324b9af5bca59188e0d411
esmaKula <111609979+esma778787@users.noreply.github.com>
2025-04-25 12:50:21 +0300

    MiniAtm
    C# programlama dili kullanılarak geliştirilmiş basit bir konsol tabanlı mini ATM uygulaması

---
+++ mini atm yap/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mini_atm_yap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*kullanıcıya nasıl bir işelem yapmak istefdiğini soracağız
+             * 1 para görüntüleme
+             * 2 para çekmek ve eğer kullanıcının çekeceği miktar bakiyesinden düşükse kullanıcıya bakiyesininn yetersiz olduğunu belirteceğiz
+             * 3 para yatırma
+             * q çıkış yapmak
+             * değer girilmediyse lüten yapmak istediğiniz işlemi belirtiniz diyecegiz*/
+             
+            
+            int bakiye = 1000;
+            Console.WriteLine("ATMye hoşgeldiniz:");
+            Console.WriteLine("yapmak istediğiniz işlemi belirtiniz:");
+            string seçim = Console.ReadLine();
+            if (seçim == "1")
+            {
+                Console.WriteLine("kullanıcı bakiyesi:" + bakiye);
+            }
+            else if (seçim == "2")
+            {
+                Console.WriteLine("lütfen çekmek istediğiniz miktarı belirtiniz");
+                int cekilecek_tutar = int.Parse(Console.ReadLine());
+            
+             if (cekilecek_tutar <= bakiye)
+
+            {
+                Console.WriteLine("kalan tutar" + (bakiye - cekilecek_tutar));
+                Console.ReadLine();
+            }
+                else
+                {
+                    Console.WriteLine();
+                }
+
+            
+               
+            }  
+        }
+    }
+}
---
+++ mini atm yap/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("mini atm yap")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("mini atm yap")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("9a9441fe-6a73-40be-b5c6-5c0e6267bf68")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
